08fc8b1790267780686440decc6d270229254d91
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-10-12 00:18:03 +0300

    Completed task 2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
 import './App.css';
 import { Message } from './components/Message.js';
 
-
 export function App(props) {
+  const [messageList, setMessageList] = useState([
+    { author: "Igor", message: "Hi" },
+    { author: "Alex", message: "Hi frend"}
+  ]);
+  const [inputValue, setInputValue] = useState("");
+  
+  useEffect (() => {
+    let i = [messageList.length - 1];
+    if(messageList[i].author === props.name) {
+      setTimeout(() => {
+        setMessageList(messageList => [...messageList, messageList={author: `Bot`, message: `Привет`}]);
+      }, 1500);
+    }
+  },[messageList,props.name]);
+
+  const cd = () => {
+    setMessageList(messageList => [...messageList, messageList={author: `${props.name}`,message: `${inputValue}`}]);
+    setInputValue('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Message nam={props.name}></Message>
-      </header>
+        <header className="App-header">
+          <div>{messageList.map((message) => <p>{`${message.author}: ${message.message}`}</p>)}</div>
+          <div>
+            <input placeholder="input" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+            <button onClick={cd}>send</button>
+          </div>
+          <Message nam={props.name}></Message>
+        </header>
     </div>
   );
 }
-
-
-
-
-
 
--- myapp/src/components/Message.js
+++ myapp/src/components/Message.js
@@ -1,3 +1,3 @@
 export const Message = (props) => {
-    return <h1 class='massage'>{props.nam}</h1>
+    return <h1 className='massage'>{props.nam}</h1>
 }
