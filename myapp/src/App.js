2852ed62416671aa52305a6cfd5f6bbb5416f69b
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-10-15 21:31:04 +0300

    Modification

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,29 +7,35 @@ import { Message } from './components/Message.js';
 
 export function App(props) {
   const [messageList, setMessageList] = useState([
-    { author: "Igor", message: "Hi" },
-    { author: "Alex", message: "Hi frend"}
+    { author: "Igor", value: "Hi" },
+    { author: "Alex", value: "Hi frend"}
   ]);
   const [inputValue, setInputValue] = useState("");
   
   useEffect (() => {
-    let i = [messageList.length - 1];
-    if(messageList[i].author === props.name) {
-      setTimeout(() => {
-        setMessageList(messageList => [...messageList, messageList={author: `Bot`, message: `Привет`}]);
+    const lastMessege = messageList[messageList.length - 1];
+    let timetId = null;
+
+    if(lastMessege?.author === props.name) {
+      timetId = setTimeout(() => {
+        setMessageList(state => [...state, {author: `Bot`, value: `Привет`}]);
       }, 1500);
     }
-  },[messageList,props.name]);
+
+    return () => clearInterval(timetId);
+  },[messageList, props.name]);
 
   const cd = () => {
-    setMessageList(messageList => [...messageList, messageList={author: `${props.name}`,message: `${inputValue}`}]);
-    setInputValue('');
+    if(inputValue) {
+      setMessageList(state => [...state, { author: `${props.name}`, value: `${inputValue}`}]);
+      setInputValue('');
+    }
   };
 
   return (
     <div className="App">
         <header className="App-header">
-          <div>{messageList.map((message) => <p>{`${message.author}: ${message.message}`}</p>)}</div>
+          <div className="chatContainer">{messageList.map((message) => <p>{`${message.author}: ${message.value}`}</p>)}</div>
           <div>
             <input placeholder="input" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
             <button onClick={cd}>send</button>
