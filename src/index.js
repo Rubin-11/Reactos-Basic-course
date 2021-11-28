1a035dd8730f7c601e0cf157db18f0e5ea139da2
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-11-29 02:43:21 +0300

    29.11.21 02:42 lesson-5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ThemeProvider, createTheme } from "@mui/material";
 import React from "react";
 import { ChatPage } from "./pages";
 import { HomePage } from "./pages";
 import "./global.css";
+import {profileReducer} from "./store"
 
 const light = createTheme({
   theme: {
@@ -13,23 +15,25 @@ const light = createTheme({
 });
 
 ReactDOM.render(
-  <BrowserRouter>
-    <ThemeProvider theme={light}>
-      <Switch>
-        <Route path="/chat">
-          <ChatPage />
-        </Route>
+  <Provider store={profileReducer}>
+    <BrowserRouter>
+      <ThemeProvider theme={light}>
+        <Switch>
+          <Route path="/chat">
+            <ChatPage />
+          </Route>
 
-        <Route path="/">
-          <HomePage />
-        </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
 
-        <Route path="*">
-          <h1>404 Ошибка!</h1>
-          <Link to="/chat">Перейти в чат</Link>
-        </Route>
-      </Switch>
-    </ThemeProvider>
-  </BrowserRouter>,
+          <Route path="*">
+            <h1>404 Ошибка!</h1>
+            <Link to="/chat">Перейти в чат</Link>
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/store/profile/actions.js
@@ -0,0 +1,5 @@
+import {EXAMPLE_ACTION} from "./types";
+
+export const exampleAction = {
+  type: EXAMPLE_ACTION
+  }
